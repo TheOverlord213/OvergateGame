8aa8000d1163c7c354f1f1fc6b19880e8f1b4c47
Blizary <rosajunior05@gmail.com>
2020-02-04 14:38:50 +0000

    pick ups base

---
+++ Assets/Scripts/PickUpObj.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObj : MonoBehaviour
+{
+    public float sphereRadius;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastForward();
+    }
+
+    void RaycastForward()
+    {
+        RaycastHit hit;
+
+        Vector3 p1 = transform.position;
+        float distanceToObstacle = 0;
+
+        // Cast a sphere wrapping character controller 10 meters forward
+        // to see if it is about to hit anything.
+        if (Physics.SphereCast(p1, sphereRadius, transform.forward, out hit, 20))
+        {
+            if(hit.transform.gameObject.tag =="Collectable")
+            {
+                Destroy(hit.transform.gameObject);
+            }
+           
+        }
+
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.red;
+        Vector3 added = new Vector3(0, 0, 20);
+        Gizmos.DrawWireSphere(transform.position+added, sphereRadius);
+    }
+}
