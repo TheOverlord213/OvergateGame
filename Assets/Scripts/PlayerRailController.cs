042445ae4c46ab1315e51dd2a76eafbfa60370e9
Sean Docherty <sean.docherty213@hotmail.com>
2020-02-18 12:47:40 +0000

    Added pause for player movement

--- Assets/Scripts/PlayerRailController.cs
+++ Assets/Scripts/PlayerRailController.cs
@@ -7,15 +7,15 @@ using UnityEngine;
 public class PlayerRailController : MonoBehaviour
 {
     public UnityEngine.AI.NavMeshAgent PlayerAgent { get; private set; }
-
-    private int playerSpeed = 3;
-    private readonly int rotationSpeed = 5;
+    public int playerSpeed;
 
     public Transform[] targetPoints;
     private int destPoint = 0;
     bool firstPointReached = false;
 
     private float playerWaitChecker = 0;
+
+    private bool movementStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,20 +29,8 @@ public class PlayerRailController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // allows the player to control their movement movement
-        //PlayerMovement();
-        RailController();
-    }
-
-    void PlayerMovement()
-    {
-        float moveHor = Input.GetAxis("Horizontal");
-        float moveVer = Input.GetAxis("Vertical");
-
-        Vector3 position = transform.position;
-        position.x += moveHor * playerSpeed * Time.deltaTime;
-        position.z += moveVer * playerSpeed * Time.deltaTime;
-        transform.position = position;
+        if (movementStarted)
+            RailController();
     }
 
     void RailController()
@@ -53,11 +41,6 @@ public class PlayerRailController : MonoBehaviour
         else
         {
             PlayerAgent.SetDestination(targetPoints[destPoint].position);
-
-            //Vector3 lookPos = targetPoints[destPoint].transform.position - PlayerAgent.transform.position;
-            //lookPos.y = 0;
-            //Quaternion targetRotation = Quaternion.LookRotation(lookPos);
-            //PlayerAgent.transform.rotation = Quaternion.Slerp(PlayerAgent.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
 
         if (!PlayerAgent.pathPending && PlayerAgent.remainingDistance < 0.5f)
@@ -78,9 +61,11 @@ public class PlayerRailController : MonoBehaviour
                 firstPointReached = true;
                 destPoint = (destPoint + 1) % targetPoints.Length;
             }
+        }         
+    }
 
-
-        }
-            
+    public void MovementBegun()
+    {
+        movementStarted = true;
     }
 }
